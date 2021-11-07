0eb6959b6ab4eda66924286e160bf7c533c84bf1
Brigitte GABAUD <brigabdsl@hotmail.com>
2021-11-07 11:42:20 +0100

    modif rate-limit

--- backend/middleware/rateLimit.js
+++ backend/middleware/rateLimit.js
@@ -6,5 +6,5 @@ exports.Limiter = rateLimit({
     windowMs: 60 * 60 * 1000, // 1 heure de pénalisation
     max: 5, // Bloque à partir de 5 requêtes
     message:
-      "Trop de comptes créés à partir de cette adresse IP, veuillez réessayer d'ici une heure."
+      "Trop de tentatives de connexion ou de création de comptes à partir de cette adresse IP, veuillez réessayer d'ici une heure."
   });
